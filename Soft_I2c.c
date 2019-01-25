0aa03adc5a3bfdc8cae66daf628b0e380d579374
wangleiei <2350235394@qq.com>
2019-01-25 15:34:32 +0800

    简化接口

--- Soft_I2c.c
+++ Soft_I2c.c
@@ -5,19 +5,13 @@ static void I2C_start(SoftI2cA*base);
 static void I2C_stop(SoftI2cA*base);
 static void I2C_Nack(SoftI2cA*base);
 static void I2C_ack(SoftI2cA*base);
+static uint8_t  I2C_SendAddr(SoftI2cA*base,uint16_t RegAddr);
 
-void Softi2cA_Init(SoftI2cA* base)
-{
-
- 	if((0 == base->set_sda_pp_static) || (0 == base->set_scl_pp_static)){
- 		return;
-	}
-
+void Softi2cA_Init(SoftI2cA* base){
 	base->set_sda_pp_static();
 	base->set_scl_pp_static();
 }
-static void I2C_start(SoftI2cA*base)
-{	
+static void I2C_start(SoftI2cA*base){	
 	
 	base->write_scl_l_static();
 	base->delayus_static(base->i2c_rate);
@@ -34,8 +28,7 @@ static void I2C_start(SoftI2cA*base)
 	base->write_scl_l_static();	
 	base->delayus_static(base->i2c_rate);
 }
-static void I2C_stop(SoftI2cA*base)
-{
+static void I2C_stop(SoftI2cA*base){
 	base->write_scl_l_static();	
 	base->delayus_static(base->i2c_rate);
 
@@ -48,8 +41,7 @@ static void I2C_stop(SoftI2cA*base)
 	base->write_sda_h_static();	
 	base->delayus_static(base->i2c_rate);
 }
-static void I2C_Nack(SoftI2cA*base)
-{
+static void I2C_Nack(SoftI2cA*base){
 	base->delayus_static(base->i2c_rate);
 	base->write_sda_h_static();
 	base->delayus_static(base->i2c_rate);
@@ -60,10 +52,8 @@ static void I2C_Nack(SoftI2cA*base)
 	base->write_scl_l_static();
 	base->delayus_static(base->i2c_rate);
 }
-static uint8_t I2C_SendAddr(SoftI2cA*base,uint16_t I2CAddr,uint16_t RegAddr,uint8_t SubaNum);
 
-static void I2C_ack(SoftI2cA*base)
-{
+static void I2C_ack(SoftI2cA*base){
 
 	base->delayus_static(base->i2c_rate);
 	base->write_sda_l_static();
@@ -77,19 +67,15 @@ static void I2C_ack(SoftI2cA*base)
 	base->delayus_static(base->i2c_rate);
 }
 // 返回0 发送成功，返回1发送失败
-uint8_t I2C_SendByte(SoftI2cA*base,uint8_t SendByte)
-{
+uint8_t I2C_SendByte(SoftI2cA*base,uint8_t SendByte){
 	uint8_t status;
 	uint16_t count;
 	
-	for(count=8; count>0;count--)
-	{
-		if((SendByte&0x80) == 0x80)
-		{
+	for(count=8; count>0;count--){
+		if((SendByte&0x80) == 0x80){
 			base->write_sda_h_static();
 		}	
-		else
-		{
+		else{
 			base->write_sda_l_static();
 		}	
 		
@@ -112,11 +98,9 @@ uint8_t I2C_SendByte(SoftI2cA*base,uint8_t SendByte)
 	status = 0;	
 	
 	count = 100;	
-	while(base->read_sda_static() == 1)
-	{
+	while(base->read_sda_static() == 1){
 		count --;
-		if(count == 0)
-		{
+		if(count == 0){
 			status = 1;	
 			break;
 		}
@@ -132,8 +116,7 @@ uint8_t I2C_SendByte(SoftI2cA*base,uint8_t SendByte)
 	
 	return(status);
 }
-uint8_t I2C_ReceiveByte(SoftI2cA*base)
-{
+uint8_t I2C_ReceiveByte(SoftI2cA*base){
 
 	
 	uint8_t count;	
@@ -143,15 +126,13 @@ uint8_t I2C_ReceiveByte(SoftI2cA*base)
 
 	base->delayus_static(20);
 
-	for(count=8; count>0; count--)
-	{
+	for(count=8; count>0; count--){
 		Rdata <<= 1;
 
 		base->write_scl_h_static();
 		base->delayus_static(base->i2c_rate);
 
-		if(base->read_sda_static() !=0)
-		{
+		if(base->read_sda_static() !=0){
 			Rdata |= 0x01;
 		}
 		
@@ -162,13 +143,14 @@ uint8_t I2C_ReceiveByte(SoftI2cA*base)
 
 	return(Rdata);
 }
-static uint8_t  I2C_SendAddr(SoftI2cA*base,uint16_t I2CAddr,uint16_t RegAddr,uint8_t SubaNum)
-{
-	I2CAddr<<=1;
-	I2CAddr&=0xFE;
-	I2C_SendByte(base,I2CAddr);
+static uint8_t  I2C_SendAddr(SoftI2cA*base,uint16_t RegAddr){
+	uint8_t temp = 0;
+	temp = base->dev_addr;
+	temp<<=1;
+	temp&=0xFE;
+	I2C_SendByte(base,temp);
 
-	if(SubaNum != 1){
+	if(base->reg_addrlen != 1){
 		I2C_SendByte(base,(uint8_t)(RegAddr>>8));			
 	}
 
@@ -177,25 +159,22 @@ static uint8_t  I2C_SendAddr(SoftI2cA*base,uint16_t I2CAddr,uint16_t RegAddr,uin
 	return 0;
 }
 
-uint8_t I2C_WriteNByte(SoftI2cA*base,uint16_t I2CAddr,uint16_t RegAddr,uint8_t *pBuffer,uint16_t length,uint8_t SubaNum)
-{
+uint8_t I2C_WriteNByte(SoftI2cA*base,uint16_t RegAddr,uint8_t *pBuffer,uint16_t length){
 	uint8_t status;
 
 	I2C_start(base);
 
-	status = I2C_SendAddr(base,I2CAddr,RegAddr,SubaNum);
+	status = I2C_SendAddr(base,RegAddr);
 
-	while(length != 0x00)
-	{
+	while(length != 0x00){
 		status = I2C_SendByte(base,*pBuffer);
 		(uint8_t*)pBuffer ++;
 		RegAddr++;
-		if ((RegAddr % 16) == 0)
-		{
+		if ((RegAddr % 16) == 0){
 			I2C_stop(base);	
 			base->delayus_static(100);
 			I2C_start(base);
-			status = I2C_SendAddr(base,I2CAddr,RegAddr,SubaNum);
+			status = I2C_SendAddr(base,RegAddr);
 		}
 		length --;
 		base->delayus_static(base->i2c_rate);
@@ -204,24 +183,23 @@ uint8_t I2C_WriteNByte(SoftI2cA*base,uint16_t I2CAddr,uint16_t RegAddr,uint8_t *
 
 	return(status);
 }
-uint8_t I2C_ReadNByte(SoftI2cA*base,uint16_t I2CAddr,uint16_t RegAddr,uint8_t *pBuffer,uint16_t length,uint8_t SubaNum)
-{	
-
+uint8_t I2C_ReadNByte(SoftI2cA*base,uint16_t RegAddr,uint8_t *pBuffer,uint16_t length){	
+	uint8_t addrtemp = 0;
 	uint8_t status;
+	addrtemp = base->dev_addr;
 
 	I2C_start(base);
 
-	status = I2C_SendAddr(base,I2CAddr,RegAddr,SubaNum);
+	status = I2C_SendAddr(base,RegAddr);
 
 	I2C_start(base);							
 	
-	I2CAddr<<=1;
-	status = I2C_SendByte(base,I2CAddr|0x01);
+	addrtemp<<=1;
+	status = I2C_SendByte(base,addrtemp|0x01);
 
 	while(length != 0x00){
 		*(uint8_t*)pBuffer = I2C_ReceiveByte(base);
-		if(length != 1)	
-		{
+		if(length != 1)	{
 			I2C_ack(base);	
 		}
 
--- Soft_I2c.h
+++ Soft_I2c.h
@@ -2,10 +2,12 @@
 #define __I2C_H_
 
 #include "stm8l15x.h"
-
+// 如果手册上写的是bit7-bit0-->0100-001rw，则该协议需要使用地址是0X21
 typedef void (*VOIDFUNC)(void);
 typedef int (*INTFPTR)(void);
 typedef void (*FUNPTRINT)(int);
+typedef enum {ADDR_8_BITS = 1,ADDR_16_BITS = 2}REGADDRLEN;
+
 typedef struct SOFT_I2C1
 {		
 	VOIDFUNC  write_sda_h_static;  //sda输出高电平
@@ -18,6 +20,8 @@ typedef struct SOFT_I2C1
 	VOIDFUNC  set_scl_pp_static;//设置scl为输入模式
 	FUNPTRINT  delayus_static;//模拟延时
 	uint32_t i2c_rate;//延时单位，用来控制i2c通信速率
+	uint16_t  dev_addr;//设备地址
+	REGADDRLEN reg_addrlen;  //寄存器的地址长度，有些设备的寄存器比较多，地址可能多大16位  
 }SoftI2cA ;
 /**********************************************************************************************************
 *	函 数 名: void Softi2cA_Init(SoftI2cA * base)
@@ -26,18 +30,20 @@ typedef struct SOFT_I2C1
 *	返 回 值: 无
 *   说    明: 
 历程：
-	SoftI2cA i2c_p2;
-	i2c_p2.write_sda_h_static = p2_sda_h;//sda输出高电平
-	i2c_p2.write_sda_l_static = p2_sda_l;//sda输出低电平
-	i2c_p2.write_scl_h_static = p2_scl_h;//scl输出高电平
-	i2c_p2.write_scl_l_static = p2_scl_l;//scl输出低电平
-	i2c_p2.read_sda_static = (INTFPTR)p2_read_sda;//读取sda电平高低，高电平返回1，低电平返回0
-	i2c_p2.set_sda_pp_static = p2_sda_pp;//设置sda为输出模式
-	i2c_p2.set_sda_in_static = p2_sda_in;//设置scl为输入模式
-	i2c_p2.set_scl_pp_static = p2_scl_pp;//设置scl为输入模式
-	i2c_p2.delayus_static = block_delayus;//模拟延时
-	i2c_p2.i2c_rate = 10;//延时单位，用来控制i2c通信速率
-	Softi2cA_Init(&i2c_p2);
+	SoftI2cA i2c_dev;
+	i2c_dev.write_sda_h_static = p2_sda_h;//sda输出高电平
+	i2c_dev.write_sda_l_static = p2_sda_l;//sda输出低电平
+	i2c_dev.write_scl_h_static = p2_scl_h;//scl输出高电平
+	i2c_dev.write_scl_l_static = p2_scl_l;//scl输出低电平
+	i2c_dev.read_sda_static = (INTFPTR)p2_read_sda;//读取sda电平高低，高电平返回1，低电平返回0
+	i2c_dev.set_sda_pp_static = p2_sda_pp;//设置sda为输出模式
+	i2c_dev.set_sda_in_static = p2_sda_in;//设置scl为输入模式
+	i2c_dev.set_scl_pp_static = p2_scl_pp;//设置scl为输入模式
+	i2c_dev.delayus_static = block_delayus;//模拟延时
+	i2c_dev.i2c_rate = 10;//延时单位，用来控制i2c通信速率
+	i2c_dev.dev_addr = 0x21;//设备地址
+	i2c_dev.reg_addrlen = ADDR_8_BITS;//寄存器地址长度
+	Softi2cA_Init(&i2c_dev);
 *********************************************************************************************************/
 void Softi2cA_Init(SoftI2cA * base);
 
@@ -62,29 +68,25 @@ uint8_t I2C_ReceiveByte(SoftI2cA * base);
 *	函 数 名: uint8_t I2C_WriteNByte(SoftI2cA * base,uint16_t I2CAddr,uint16_t RegAddr,uint8_t *pBuffer,uint16_t length,uint8_t SubaNum);
 *	功能说明: 
 *	传    参: base:结构体指针
-			  I2CAddr：该i2c的设备地址，
 			  RegAddr：要操作的寄存器地址
 			  pBuffer：发送数据缓冲区
 			  length：发送数据长度
-			  SubaNum：是寄存器地址长度1：单字节，2：双字节
 *	返 回 值: 0：成功，1失败
 *   说    明: 
 *********************************************************************************************************/
-uint8_t I2C_WriteNByte(SoftI2cA * base,uint16_t I2CAddr,uint16_t RegAddr,uint8_t *pBuffer,uint16_t length,uint8_t SubaNum);
+uint8_t I2C_WriteNByte(SoftI2cA * base,uint16_t RegAddr,uint8_t *pBuffer,uint16_t length);
 // SubaNum 是寄存器地址长度，分为双字节地址，单字节地址
 /**********************************************************************************************************
 *	函 数 名: uint8_t I2C_ReadNByte(SoftI2cA * base,uint16_t I2CAddr,uint16_t RegAddr,uint8_t *pBuffer,uint16_t length,uint8_t SubaNum);
 *	功能说明: 
 *	传    参: base:结构体指针
-			I2CAddr：该i2c的设备地址，
 			RegAddr：要操作的寄存器地址
 			pBuffer：接收数据缓冲区
 			length：接收数据长度
-			SubaNum：是寄存器地址长度1：单字节，2：双字节
 *	返 回 值: 0：成功，1失败
 *   说    明: 
 *********************************************************************************************************/
-uint8_t I2C_ReadNByte(SoftI2cA * base,uint16_t I2CAddr,uint16_t RegAddr,uint8_t *pBuffer,uint16_t length,uint8_t SubaNum);
+uint8_t I2C_ReadNByte(SoftI2cA * base,uint16_t RegAddr,uint8_t *pBuffer,uint16_t length);
 		
 
 #endif
